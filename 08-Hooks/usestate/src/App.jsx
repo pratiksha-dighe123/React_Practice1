5b44c6e9f2ad67e8195e0731280a95b458a72462
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-27 13:38:21 +0530

    Practice hooks

--- 08-Hooks/usestate/src/App.jsx
+++ 08-Hooks/usestate/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const App = () => {
 
+  const [num, setNum] = useState(10)
+
+  function changeNum(){
+    setNum(20);
+  }
 
   return (
     <div>
-      Hello guys
+      <h1>The value of num is {num}</h1>
+      <button onClick={changeNum}>Change num</button>
     </div>
   )
 }
