d50656fffeefa12abe6c605eceb5447a8cf963a1
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-27 16:15:21 +0530

    Form handling

--- 08-Hooks/usestate/src/App.jsx
+++ 08-Hooks/usestate/src/App.jsx
@@ -3,16 +3,27 @@ import { useState } from "react"
 
 const App = () => {
 
-  const [num, setNum] = useState(10)
+  const [num, setNum] = useState(0);
 
-  function changeNum(){
-    setNum(20);
-  }
+      function increase(){
+        setNum(num + 1);
+      } 
+
+      function decrease(){
+        setNum(num - 1);
+      }
+
+      function jumpBy5(){
+        setNum(num + 5);
+      }
+
 
   return (
     <div>
-      <h1>The value of num is {num}</h1>
-      <button onClick={changeNum}>Change num</button>
+      <h1>{num}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={jumpBy5}>Jump by 5</button>
     </div>
   )
 }
